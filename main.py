08fb3bd159aed147c56c657cdeccc58588065c38
RevealedSoulEven <anshumaurya000@gmail.com>
2024-07-15 00:44:36 +0530

    v1.001
    idk how long will it take to run for the first time huuuhhhhhhh

--- main.py
+++ main.py
@@ -22,82 +22,96 @@ def combine(source):
 
 def deobfuscate(smali):
     global array_declared, arrayname, cmds
-    try:
-        for index in range(len(smali)):
-            line = smali[index]
-            op__ = line.split(" ")
+    # try:
+    for index in range(len(smali)):
+        line = smali[index]
+        op__ = line.split(" ")
+        
+        if "new-array" in line and "[C" in line:
+            arraysize__ = op__[2].replace(",",'')
+            arrayname = op__[1].replace(",","")
+            ##### get size
+            if "const/" in smali[index-1]:
+                op__2 = smali[index-1].split(" ")
+                arraysize = int(op__2[2], 16)
+                cmds.append(f"{arrayname} = [None] * {arraysize}")
+                array_declared = True
+        
             
-            if "new-array" in line and "[C" in line:
-                arraysize__ = op__[2].replace(",",'')
-                arrayname = op__[1].replace(",","")
-                ##### get size
-                if "const/" in smali[index-1]:
+        if array_declared:
+            
+            if "xor-int/lit16" in line:
+                out__ = op__[1].replace(",","")
+                to_be_added = op__[2].replace(",","")
+                opval__ = op__[3]
+            
+                if "const" in smali[index-1]:
                     op__2 = smali[index-1].split(" ")
-                    arraysize = int(op__2[2], 16)
-                    cmds.append(f"{arrayname} = [None] * {arraysize}")
-                    array_declared = True
+                    op2val__ = op__2[2]
+                    if op__2[1].replace(",",'') == to_be_added:
+                        cmds.append(f"{out__} = {op2val__} ^ {opval__}")
             
+                elif "aget-char" in smali[index-1]:
+                    op__2 = smali[index-1].split(" ")
+                    temp__ = cmds[-1]
+                    cmds.pop()
+                    op__temp = temp__.split(" ")
+                    if op__2[1].replace(",",'') == to_be_added:
+                        xcc = temp__.split(" = ")[1]
+                        cmds.append(f"{out__} = ord({xcc}) ^ {opval__}")
+                                
+                        
+            if "int-to-char" in line:
+                cmds.append(f"{op__[1].replace(',','')} = chr({op__[2]})")
                 
-            if array_declared:
+                combine("int-to-char")
+            
+            
+            if "aput-char" in line:
+                ele__ = op__[1].replace(",","")
+                arr__ = op__[2].replace(",","")
+                index__ = op__[3]
                 
-                if "xor-int/lit16" in line:
-                    out__ = op__[1].replace(",","")
-                    to_be_added = op__[2].replace(",","")
-                    opval__ = op__[3]
+                if "const" in smali[index-1]:
+                    op__2 = smali[index-1].split(" ")
+                    if op__2[1].replace(",",'') == index__:
+                        cmds.append(f"{arr__}[{int(op__2[2], 16)}] = {ele__}")
+                        
+                        combine("aput-char")
+            
+            
+            if "aget-char" in line:
+                ele__ = op__[1].replace(",","")
+                arr__ = op__[2].replace(",","")
+                index__ = op__[3]
                 
-                    if "const" in smali[index-1]:
-                        op__2 = smali[index-1].split(" ")
-                        op2val__ = op__2[2]
-                        if op__2[1].replace(",",'') == to_be_added:
-                            cmds.append(f"{out__} = {op2val__} ^ {opval__}")
-                
-                    elif "aget-char" in smali[index-1]:
-                        op__2 = smali[index-1].split(" ")
-                        temp__ = cmds[-1]
-                        cmds.pop()
-                        op__temp = temp__.split(" ")
-                        if op__2[1].replace(",",'') == to_be_added:
-                            xcc = temp__.split(" = ")[1]
-                            cmds.append(f"{out__} = ord({xcc}) ^ {opval__}")
-                                    
-                            
-                if "int-to-char" in line:
-                    cmds.append(f"{op__[1].replace(',','')} = chr({op__[2]})")
-                    
-                    combine("int-to-char")
-                
-                
-                if "aput-char" in line:
-                    ele__ = op__[1].replace(",","")
-                    arr__ = op__[2].replace(",","")
-                    index__ = op__[3]
-                    
-                    if "const" in smali[index-1]:
-                        op__2 = smali[index-1].split(" ")
-                        if op__2[1].replace(",",'') == index__:
-                            cmds.append(f"{arr__}[{int(op__2[2], 16)}] = {ele__}")
-                            
-                            combine("aput-char")
-                
-                
-                if "aget-char" in line:
-                    ele__ = op__[1].replace(",","")
-                    arr__ = op__[2].replace(",","")
-                    index__ = op__[3]
-                    
-                    if "const" in smali[index-1]:
-                        op__2 = smali[index-1].split(" ")
-                        if op__2[1].replace(",",'') == index__:
-                            cmds.append(f"{ele__} = {arr__}[{int(op__2[2], 16)}]")
-                            
-                
-                if "Ljava/lang/String;->intern()" in line:
+                if "const" in smali[index-1]:
+                    op__2 = smali[index-1].split(" ")
+                    if op__2[1].replace(",",'') == index__:
+                        cmds.append(f"{ele__} = {arr__}[{int(op__2[2], 16)}]")
+                        
+                        
+            
+            if "Ljava/lang/String;->intern()" in line:
+                if len(cmds) > arraysize:
                     #### close everypthing
                     cmds.append(f"result_str = ''.join({arrayname})")
                     # cmds.append("""print("'" + result_str + "'")""")
                     
-                    _______ = ""
+                    #### fixing the chr() range (req for java to python)
+                    c_m_d = []
                     for i in cmds:
+                        if "(chr(" in i:
+                            _o_ = i.replace("(chr(", "(chr((").replace("))", ") & 0x10FFFF))")
+                            c_m_d.append(_o_)
+                        else:
+                            c_m_d.append(i)
+                            
+                    
+                    
+                    
+                    _______ = ""
+                    for i in c_m_d:
                         _______ += i + "\n"
 
                     ret__ = {}
@@ -108,8 +122,11 @@ def deobfuscate(smali):
                     arrayname = ""
 
                     new_string_ = ret__["result_str"]
-    except:
-        return smali
+                else:
+                    return smali
+    # except:
+    #     print(f"-------------------------:1111111111")
+    #     return smali
             
     try:
         new_lines = []
@@ -130,6 +147,7 @@ def deobfuscate(smali):
                 new_lines.append(line)
                 
     except:
+        print("-------------------------:try22222222")
         new_lines = smali
         
     # print("----------------------------------------------------------------")
@@ -148,9 +166,9 @@ def deobfuscate(smali):
                 
             
             
-            
 
 def process_file(filepath):
+    print(filepath)
     curr_smali = []
     try:
         with open(filepath, "r", encoding="utf-8") as f:
@@ -188,9 +206,88 @@ def process_file(filepath):
 
     return curr_smali, None
 
+def process_folder(folder, folderout):
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file.endswith(".smali"):
+                input_filepath = os.path.join(root, file)
+                
+                relative_path = os.path.relpath(input_filepath, folder)
+                output_filepath = os.path.join(folderout, relative_path)
+                if os.path.exists(output_filepath):
+                    print(f"File {output_filepath} already exists. Skipping.")
+                else:
+                    os.makedirs(os.path.dirname(output_filepath), exist_ok=True)
+                    
+                    new_lines, error = process_file(input_filepath)
+                    
+                    if error:
+                        # Copy the original file to the output directory
+                        shutil.copyfile(input_filepath, output_filepath)
+                        print(f"Error processing {input_filepath}: {error}. Copied original file.")
+                    elif new_lines:
+                        # Write the modified lines to the output file
+                        with open(output_filepath, "w", encoding="utf-8") as f:
+                            for line in new_lines:
+                                f.write(line + "\n")
+                        print(f"Wrote : {output_filepath}")
+
+# Example usage
+folder = "smali3"
+folderout = "smali22"
+process_folder(folder, folderout)
+
+
+         
+         
+         
+         
+         
+         
+            
+"""
+def process_file(filepath):
+    curr_smali = []
+    try:
+        with open(filepath, "r", encoding="utf-8") as f:
+            for line in f.readlines():
+                line = line.replace("\n", "")
+                if len(line.split()) > 0:
+                    curr_smali.append(line.strip())
+    except (UnicodeDecodeError, IOError) as e:
+        print("-------------------------:process_file")
+        return None, e
+    
+    found__ = False
+    start_line = -1
+    curr_line = -1
+    changes = []
+
+    for line in curr_smali:
+        curr_line += 1
+        if not found__:
+            if "new-array" in line and "[C" in line:
+                start_line = (curr_line - 3) if (curr_line - 3) >= 0 else 0
+                found__ = True
+        
+        elif "Ljava/lang/String;->intern()" in line:
+            end_line = curr_line + 2
+            new_lines = deobfuscate(curr_smali[start_line:end_line])
+            changes.append((start_line, end_line, new_lines))
+            found__, start_line, end_line = False, -1, -1
+
+        elif ".end method" in line and found__:
+            found__ = False
+            start_line = -1
+
+    for start_line, end_line, new_lines in reversed(changes):
+        curr_smali[start_line:end_line] = new_lines
+
+    return curr_smali, None
+
 def process_file_wrapper(input_filepath, output_filepath):
     if os.path.exists(output_filepath):
-        print(f"File {output_filepath} already exists. Skipping.")
+        # print(f"File {output_filepath} already exists. Skipping.")
         return
     
     new_lines, error = process_file(input_filepath)
@@ -198,7 +295,7 @@ def process_file_wrapper(input_filepath, output_filepath):
     if error:
         # Copy the original file to the output directory
         shutil.copyfile(input_filepath, output_filepath)
-        print(f"Error processing {input_filepath}: {error}. Copied original file.")
+        # print(f"Error processing {input_filepath}: {error}. Copied original file.")
     else:
         # Write the modified lines to the output file
         with open(output_filepath, "w", encoding="utf-8") as f:
@@ -206,7 +303,7 @@ def process_file_wrapper(input_filepath, output_filepath):
                 f.write(line + "\n")
         print(f"wrote: {output_filepath}")
 
-def process_folder(folder, folderout, max_workers=50):
+def process_folder(folder, folderout, max_workers=4):
     tasks = []
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         for root, dirs, files in os.walk(folder):
@@ -226,10 +323,4 @@ def process_folder(folder, folderout, max_workers=50):
                 future.result()
             except Exception as e:
                 print(f"Error processing a file: {e}")
-                    
-
-# Example usage
-folder = "smali3"
-folderout = "smali22"
-process_folder(folder, folderout)
-
+"""                    
